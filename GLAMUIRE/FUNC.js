b5b2960fc6075b3b4fa1bc17f1ef39d710f01d13
Shantelle Sotelo (Stella) <shantelle.sotelo2804@gmail.com>
2024-05-27 20:52:20 +0800

    Add files via upload

---
+++ GLAMUIRE/FUNC.js
@@ -0,0 +1,201 @@
+var attempt = 0; 
+function log_out(){
+         window.location.href = "login.html";
+}
+
+function LogIn(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username == "shantelle.sotelo2804@gmail.com" && password == "1234"){
+        window.location.href = "home.html";
+        return true;
+    } else {
+        attempt ++;
+    if (attempt == 3) {
+        alert("Cannot login. Attempts exceeded!");
+        document.getElementById("btn_logIn").disabled = true;
+        document.getElementById("username").disabled = true;
+        document.getElementById("password").disabled = true;
+        document.getElementById("username").value = null;
+        document.getElementById("password").value = null;
+        return false;
+    } else {
+         alert("Incorrect username or password.")
+        return false;
+    }
+    }
+
+    function restPass() {
+    }
+}
+	
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    var navbar = document.getElementById("navbar");
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+}
+
+function tabtrigger(){
+	document.body.classList.toggle('carttrigger');
+}
+
+function Items(){
+    window.location.href = "items.html";
+}
+
+let boughtitems = [];
+let totalprice = 0;
+
+function addtocart(event){
+    const button = event.target;
+    const product = button.closest('.product');
+    const name = product.querySelector('h3').textContent;
+
+    if (!boughtitems.includes(name)){
+        boughtitems.push(name);
+
+        const count = document.querySelector('.itemcount');
+        const counttext = count.textContent;
+        const parsedcount = parseInt(counttext);
+        const update = parsedcount + 1;
+        count.textContent = update;
+
+        const image = product.querySelector('img').src;
+        const price = product.querySelector('.price').textContent;
+
+        const cartcontainer = document.createElement('div');
+        cartcontainer.classList.add('itemvariables');
+
+        const item_image = document.createElement('div');
+        item_image.classList.add('cartitemimg');
+        const img = document.createElement('img');
+        img.src = image;
+        item_image.appendChild(img);
+
+        const quantity = document.createElement('div');
+        quantity.classList.add('c_itemquantity');
+        quantity.innerHTML = `
+            <span><input type="button" value="<" class="decrement" onclick="decrement(event)"></span>
+            <span class="itemquantity">1</span>
+            <span><input type="button" value=">" class="increment" onclick="increment(event)"></span>
+        `;
+
+        const item_name = document.createElement('div');
+        item_name.classList.add('c_itemname');
+        item_name.textContent = name;
+
+        const item_price = document.createElement('div');
+        item_price.classList.add('c_itemprice');
+        item_price.textContent = price;
+
+        const del = document.createElement('div');
+        del.classList.add('del');
+        del.innerHTML = `
+            <img src="Designs/Del.png" onclick="del(event)" id="${name}">
+        `;
+    
+        cartcontainer.appendChild(item_image);
+        cartcontainer.appendChild(quantity);
+        cartcontainer.appendChild(item_name);
+        cartcontainer.appendChild(item_price);
+        cartcontainer.appendChild(del);
+
+        const cart = document.querySelector('.listCart');
+        cart.appendChild(cartcontainer);
+
+        const total_price = price.replace(/₱|,/g, "");
+        const totalparsed = parseFloat(total_price);
+        totalprice += totalparsed;
+        decimal();
+    }
+
+}
+
+function del(event){
+    const button = event.target;
+    const buttonid = button.id;
+
+    for (let i = 0; i < boughtitems.length; i++) {
+        if (boughtitems[i] === buttonid){
+            boughtitems.splice(i, 1);
+        }
+    } 
+
+    const itemvariables = button.closest('.itemvariables');
+    const itemprice = itemvariables.querySelector('.c_itemprice');
+    const textcontentprice = itemprice.textContent;
+    let price = textcontentprice.replace(/₱|,/g, "");
+    const parsedprice = parseFloat(price);
+
+    const quantitydiv = itemvariables.querySelector('.itemquantity')
+    const quantitytext = quantitydiv.textContent;
+    const quantityparse = parseInt(quantitytext);
+    const itemtotal = quantityparse * parsedprice;
+
+    totalprice -= itemtotal;
+
+    itemvariables.remove();
+
+    const count = document.querySelector('.itemcount');
+        const counttext = count.textContent;
+        const parsedcount = parseInt(counttext);
+        const update = parsedcount - 1;
+        count.textContent = update;
+
+    decimal();
+}
+
+function decimal(){
+    const pricetext = document.querySelector('.total')
+    const fixedprice = totalprice.toFixed(2);
+    pricetext.textContent = fixedprice;
+}
+
+function increment(event){
+    const button = event.target;
+    const nearquantity = button.closest('.c_itemquantity');
+    const quantity_text = nearquantity.querySelector('.itemquantity');
+
+    let num = parseInt(quantity_text.textContent);
+        num++;
+        quantity_text.textContent = num;
+
+    const itemvariables = button.closest('.itemvariables');
+    const c_itemprice = itemvariables.querySelector('.c_itemprice');
+    const price_text = c_itemprice.textContent;
+    const price = price_text.replace(/₱|,/g, "");
+    const parsedprice = parseFloat(price);
+
+    totalprice += parsedprice;
+
+    decimal();
+
+}
+
+function decrement(event){
+    const button = event.target;
+    const nearquantity = button.closest('.c_itemquantity');
+    const quantity_text = nearquantity.querySelector('.itemquantity');
+
+    const itemvariables = button.closest('.itemvariables');
+    const c_itemprice = itemvariables.querySelector('.c_itemprice');
+    const price_text = c_itemprice.textContent;
+    const price = price_text.replace(/₱|,/g, "");
+    const parsedprice = parseFloat(price);
+
+    let num = parseInt(quantity_text.textContent);
+    if (num > 1){
+        num--;
+        quantity_text.textContent = num;
+        totalprice -= parsedprice;
+    }
+
+    decimal();
+}
+	
